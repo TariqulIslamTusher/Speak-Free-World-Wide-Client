ed6796d4bb415360ee69d926ca7f2a853eb9195b
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-06-08 11:00:12 +0600

    .env file implemented

--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -4,20 +4,17 @@ const DashBoard = () => {
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <div className="drawer-content flex flex-col items-center bg-slate-50 justify-center ">
                 {/* Page content here */}
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
                     <div>
                         this is container area
                     </div>
-
-
-                </label>
+                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open Drawer</label>
 
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                <ul className="menu p-4 w-64 h-full bg-yellow-100 text-base-content">
                     {/* Sidebar content here */}
                     <li><a>Sidebar Item 1</a></li>
                     <li><a>Sidebar Item 2</a></li>
--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCbCbpwoc-GMxl6mc3Gc558yEf8sCzvkX0",
-  authDomain: "speak-free-world-wide-791e6.firebaseapp.com",
-  projectId: "speak-free-world-wide-791e6",
-  storageBucket: "speak-free-world-wide-791e6.appspot.com",
-  messagingSenderId: "665596657081",
-  appId: "1:665596657081:web:6031c963773d886fbfca68"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
